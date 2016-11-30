8db5b55a79f4998c3c3a8214f7a5da00ffa2e740
glynester <glynester@hotmail.com>
2016-11-30 09:04:05 +0000

    Added test for invalid date

--- spec/transactions_spec.rb
+++ spec/transactions_spec.rb
@@ -1,24 +1,25 @@
 require 'account'
 
 describe Deposit do
-
-    context 'generate a new transaction' do
-
+  context 'generate a new transaction' do
     subject {described_class.new(220, "5/9/2016")}
-
     it 'has the expected value' do
       expect(subject.amnt).to eq 220
+    end
+  end
+
+  context 'generate a new transaction with an invalid date' do
+    subject {described_class.new(220, "35/9/2016")}
+    it 'has the expected value' do
+      expect(subject.amnt).to output "==> Sorry, the date \"35/9/2016\" is invalid <=="
     end
   end
 
 end
 
 describe Withdrawal do
-
-    context 'generate a new transaction' do
-
+  context 'generate a new transaction' do
     subject {described_class.new(220, "5/9/2016")}
-
     it 'has the expected value' do
       expect(subject.date).to eq "05/09/2016"
     end
