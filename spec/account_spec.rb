f0ea511d506122a983cf69ca897b9593fb8e964e
glynester <glynester@hotmail.com>
2016-11-30 08:28:20 +0000

    Added tests

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -9,7 +9,7 @@ describe Account do
   deposit_1 = Deposit.new(50.01)
   withdrawal_1 = Withdrawal.new(75.66)
 
-  context 'With max balance on card' do
+  context 'Add transactions' do
     before do
       # subject.add_trans(:deposit_1, amnt: 50.01, trans_type: "deposit")
       # subject.add_trans(:withdrawal_1, amnt: 75.66, trans_type: "withdrawal")
@@ -20,6 +20,15 @@ describe Account do
     it 'shows correct balance' do
       expect(subject.balance).to eq -25.65
     end
+
+    # date        || credit||  debit|| balance
+    # 30/11/2016  ||       ||  75.66||  -25.65
+    # 30/11/2016  ||  50.01||       ||   50.01
+
+    specify { expect { subject.statement }.to output(
+    "date        || credit||  debit|| balance\n30/11/2016  ||       ||  75.66||  -25.65\n30/11/2016  ||  50.01||       ||   50.01\n"
+    ).to_stdout }
+
   end
 
   end
